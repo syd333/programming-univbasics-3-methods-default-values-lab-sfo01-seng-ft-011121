7600e6c1f21aa7d852d6f9e838acfc1f404e9a71
sydney miranda <sydmiranda@icloud.com>
2020-12-17 16:04:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,8 @@
 def meal_choice (veg1, veg2, protein="tofu")
   puts "What a nutritious meal!"
   meal ="A plate of #{protein} with #{veg1} and #{veg2}."
-  puts meal 
+  puts meal
+  meal
 end
 
 puts meal_choice("brussel sprouts", "carrots")
